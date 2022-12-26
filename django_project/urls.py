489a821b38d435aeee1e1838352839c6f0b8a1c3
Muntashir Al Arefin <101044582+muntashiralarefin@users.noreply.github.com>
2022-12-26 18:20:19 +0000

    Nav pages set & DB model draft-1

--- django_project/urls.py
+++ django_project/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls, name='sign-in'),
+    path('', include('dash.urls')),
 ]

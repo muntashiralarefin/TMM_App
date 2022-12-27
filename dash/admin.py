31d2b6347820dd5c2062fc5a00e0bcc948dcfbcd
Muntashir Al Arefin <101044582+muntashiralarefin@users.noreply.github.com>
2022-12-27 06:06:47 +0000

    v1.02: Page loading issue solve & import-export option add in district model

--- dash/admin.py
+++ dash/admin.py
@@ -1,10 +1,19 @@
 from django.contrib import admin
+from import_export.admin import ImportExportModelAdmin
 from .models import *
 
 # # Register your models here.
-admin.site.register([Tourist, Feedback, Sex, Occupation, Education])
+admin.site.register([Tourist, Feedback, Occupation, Education])
 
-admin.site.register([District, Tour_type, Travel_interest, Travel_season])
+admin.site.register([Tour_type, Travel_interest, Travel_season])
 
 admin.site.register([CBT_attraction, CBT_facility, Vacation_package])
 
+# class SexAdmin(ImportExportModelAdmin):
+#   pass
+
+
+# admin.site.register([Sex, SexAdmin])
+@admin.register(Sex, District)
+class ViewAdmin(ImportExportModelAdmin):
+    pass
